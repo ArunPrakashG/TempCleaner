9e3c1780f957d2c954ec6c4ada5edeeb150af881
Arun Prakash <arun.prakash.456789@gmail.com>
2019-02-21 11:48:35 +0530

    Update Program.cs

--- TempCleaner/Program.cs
+++ TempCleaner/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Runtime.InteropServices;
+using System.Security.Principal;
 using System.Threading.Tasks;
 
 namespace TempCleaner
@@ -9,20 +10,24 @@ namespace TempCleaner
     {  
         static void Main(string[] args)
         {
-            try
+            if (!IsAdministrator())
             {
-                var UserTemp = Environment.GetEnvironmentVariable("TEMP");
-                var SystemTemp = Environment.GetEnvironmentVariable("TEMP", EnvironmentVariableTarget.Machine);
-                Directory.Delete(UserTemp, true);
-                Directory.Delete(SystemTemp, true);
+                ClearUserTemp();
                 ClearRecycleBin();
             }
-            catch (Exception e)
+            else
             {
-                Console.WriteLine(e);
-                Task.Delay(5000).Wait();
-                Environment.Exit(0);
-            }            
+                ClearSystemTemp();
+                ClearUserTemp();
+                ClearRecycleBin();
+            }
+        }
+
+        public static bool IsAdministrator()
+        {
+            var identity = WindowsIdentity.GetCurrent();
+            var principal = new WindowsPrincipal(identity);
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
         }
 
         enum RecycleFlags : uint
@@ -30,6 +35,70 @@ namespace TempCleaner
             SHRB_NOCONFIRMATION = 0x00000001,
             SHRB_NOPROGRESSUI = 0x00000002,
             SHRB_NOSOUND = 0x00000004
+        }
+
+        private static void ClearSystemTemp()
+        {
+            try
+            {
+                var SystemTemp = Environment.GetEnvironmentVariable("TEMP", EnvironmentVariableTarget.Machine);
+                var systemTempFiles = Directory.GetFiles(SystemTemp);
+
+                foreach (var fil in systemTempFiles)
+                {
+                    try
+                    {
+                        File.SetAttributes(fil, FileAttributes.Normal);
+                        
+                        File.Delete(fil);
+                    }
+                    catch (Exception e)
+                    {
+                        if(e is UnauthorizedAccessException)
+                        {
+                            continue;
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Task.Delay(5000).Wait();
+                Environment.Exit(0);
+            }
+        }
+
+        private static void ClearUserTemp()
+        {
+            try
+            {
+                var UserTemp = Environment.GetEnvironmentVariable("TEMP");
+                var userTempFiles = Directory.GetFiles(UserTemp);
+
+                foreach (var file in userTempFiles)
+                {
+                    try
+                    {
+                        File.SetAttributes(file, FileAttributes.Normal);
+                        File.Delete(file);
+                    }
+                    catch (Exception e)
+                    {
+                        if(e is UnauthorizedAccessException)
+                        {
+                            continue;
+                        }
+                    }
+                    
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Task.Delay(5000).Wait();
+                Environment.Exit(0);
+            }
         }
 
         [DllImport("Shell32.dll", CharSet = CharSet.Unicode)]
