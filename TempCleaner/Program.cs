86cfa810fd3d6a706283291abeceba6642e7b913
Arun Prakash <arun.prakash.456789@gmail.com>
2019-02-21 11:10:24 +0530

    Initial Commit

---
+++ TempCleaner/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace TempCleaner
+{
+    class Program
+    {  
+        static void Main(string[] args)
+        {
+            // Define Root path
+            var UserTemp = Environment.GetEnvironmentVariable("TEMP");
+            var SystemTemp = Environment.GetEnvironmentVariable("TEMP", EnvironmentVariableTarget.Machine);            
+
+            try
+            {
+                Directory.Delete(UserTemp, true);
+                Directory.Delete(SystemTemp, true);                
+            }
+            catch (Exception)
+            {
+
+            }            
+        }
+    }       
+    
+}
